cf8a405ec2b45be904fbfd07e21aa0c10383bc7a
syukrie <syukrie77@gmail.com>
2020-09-08 13:09:10 +0700

    Updates

---
+++ component---src-pages-404-js-a2966b01cf8af7480193.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),l=n.n(a),u=n("Zttt"),o=n("9CUm");e.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a2966b01cf8af7480193.js.map
